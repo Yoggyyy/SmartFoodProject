4191ca440897b40b6f6fe7d58da49a14b270ada9
Yoggyyyyyy <jsantorupv@gmail.com>
2025-05-24 00:16:55 +0200

    AuthJS da problemas astro.confg todo mal

--- smartfood-frontend/astro.config.mjs
+++ smartfood-frontend/astro.config.mjs
@@ -1,17 +1,25 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
-import clerk from "@clerk/astro";
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from "@astrojs/tailwind";
+import auth from 'auth-astro';
+import Google from "auth/core/providers/google";
 
 // https://astro.build/config
-
 export default defineConfig({
-  integrations: [clerk()],
+  integrations: [
+    tailwind(),
+    auth({
+      providers: [
+        Google({
+            clientId: import.meta.env.GOOGLE_CLIENT_ID,
+            clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    })
+  ],
+  
   adapter: node({ mode: "standalone" }),
-  vite: {
-    plugins: [tailwindcss()]
-  },
   publicDir: 'public',
   srcDir: 'src',
   outDir: 'dist',
